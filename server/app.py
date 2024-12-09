79bfd12d3c6bc6a781f20c27fa41786c0a13ee4c
Michael Akoko <akokomichael37@gmail.com>
2024-12-09 23:54:03 +0300

    include changes

--- server/app.py
+++ server/app.py
@@ -1,9 +1,5 @@
-# server/app.py
-#!/usr/bin/env python3
-
 from flask import Flask
 from flask_migrate import Migrate
-
 from models import db
 
 app = Flask(__name__)
@@ -12,8 +8,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
-
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
